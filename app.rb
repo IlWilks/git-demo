15399276db2e8d725a06285b66fe93aff7b944d1
Ian <ianlloydwilkinson@gmail.com>
2020-11-12 11:16:01 -0700

    added basic menu functionality to class

--- app.rb
+++ app.rb
@@ -1,5 +1,29 @@
 require_relative 'menu.rb'
 
-menu_test = Menu.new
 
-puts menu_test.class
+test_menu = {
+  message: "Please select an option:",
+  options: ['add contact', 'update contact', 'exit']
+}
+
+menu_test = Menu.new(test_menu)
+
+menu_test.print
+
+#generally have a message Please select an option or choose game
+#list of options
+#user selects one of the options
+
+
+#Please select and option
+#1) option1
+#2) option2
+#3) option3
+#4) exit
+#
+#
+#
+
+
+#want the user to see all options and select one
+
--- menu.rb
+++ menu.rb
@@ -1,5 +1,17 @@
 class Menu
-  def initialize  
+  def initialize(menu)
+    @menu = menu
   end
+
+  def print
+    puts @menu[:message]
+    #loop over menu options with logic
+    @menu[:options].each_with_index do |x, index|
+      puts "#{index+1} #{x}"
+    end
+
+  end
+
 end
+
 
